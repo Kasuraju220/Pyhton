043fc1f30e5c1273dc8bbc6a63c4e5db2d21084c
unknown <pkasuraju447@gmail.com>
2024-08-25 16:45:24 +0530

    Added the code into repo.

---
+++ python/Pattern.py
@@ -0,0 +1,23 @@
+num = 5
+num_list = [[0 for x in range(num)] for y in range(num)]
+low=0
+high=num-1
+count=int((num+1)/2)
+
+for i in range(count):
+    for j in range(low,high+1):
+        num_list[i][j]=num
+    for j in range(low+1,high+1):
+        num_list[j][high]=num
+    for j in range(high-1,low-1,-1):
+        num_list[high][j]=num
+    for j in range(high-1,low,-1):
+        num_list[j][low]=num
+    low=low+1
+    high=high-1
+    num=num-1
+
+for i in range(5):
+    for j in range(5):
+        print(num_list[i][j],end="\t")
+    print()
